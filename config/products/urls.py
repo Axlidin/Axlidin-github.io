d0354f7de0f08b53af1fc7cecfba457f60b027a0
Axliddin <101123936+Axlidin@users.noreply.github.com>
2023-12-31 16:37:14 +0500

    first webApp bot

--- config/products/urls.py
+++ config/products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import Products_show
 
 urlpatterns = [
-    path('', Products_show.as_view(), name='base')
+    path('', Products_show.as_view(), name='index')
 ]
