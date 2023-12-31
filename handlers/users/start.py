ae2a84241eeddc7dd5af77a1cef395ae46d7e743
Axliddin <101123936+Axlidin@users.noreply.github.com>
2023-12-31 16:27:22 +0500

    first webApp bot

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -26,31 +26,31 @@ async def start(message: types.Message):
     await message.answer('Web App!',
                            reply_markup=markup)
 
-PRICE = {
-    '1': [types.LabeledPrice(label='Item1', amount=100000)],
-    '2': [types.LabeledPrice(label='Item2', amount=200000)],
-    '3': [types.LabeledPrice(label='Item3', amount=300000)],
-    '4': [types.LabeledPrice(label='Item4', amount=400000)],
-    '5': [types.LabeledPrice(label='Item5', amount=500000)],
-    '6': [types.LabeledPrice(label='Item6', amount=600000)]
-}
-
-@dp.message_handler(content_types='web_app_data')
-async def buy_process(web_app_message):
-    await bot.send_invoice(web_app_message.chat.id,
-                           title='Laptop',
-                           description='Description',
-                           provider_token='398062629:TEST:999999999_F91D8F69C042267444B74CC0B3C747757EB0E065',
-                           currency='UZS',
-                           need_email=True,
-                           prices=PRICE[f'{web_app_message.web_app_data.data}'],
-                           start_parameter='example',
-                           payload='some_invoice')
-
-@dp.pre_checkout_query_handler(lambda query: True)
-async def pre_checkout_process(pre_checkout: types.PreCheckoutQuery):
-    await bot.answer_pre_checkout_query(pre_checkout.id, ok=True)
-
-@dp.message_handler(content_types=types.ContentType.SUCCESSFUL_PAYMENT)
-async def successful_payment(message: types.Message):
-    await bot.send_message(message.chat.id, 'Toʻlov muvaffaqiyatli boʻldi!')
+# PRICE = {
+#     '1': [types.LabeledPrice(label='Item1', amount=100000)],
+#     '2': [types.LabeledPrice(label='Item2', amount=200000)],
+#     '3': [types.LabeledPrice(label='Item3', amount=300000)],
+#     '4': [types.LabeledPrice(label='Item4', amount=400000)],
+#     '5': [types.LabeledPrice(label='Item5', amount=500000)],
+#     '6': [types.LabeledPrice(label='Item6', amount=600000)]
+# }
+#
+# @dp.message_handler(content_types='web_app_data')
+# async def buy_process(web_app_message):
+#     await bot.send_invoice(web_app_message.chat.id,
+#                            title='Laptop',
+#                            description='Description',
+#                            provider_token='398062629:TEST:999999999_F91D8F69C042267444B74CC0B3C747757EB0E065',
+#                            currency='UZS',
+#                            need_email=True,
+#                            prices=PRICE[f'{web_app_message.web_app_data.data}'],
+#                            start_parameter='example',
+#                            payload='some_invoice')
+#
+# @dp.pre_checkout_query_handler(lambda query: True)
+# async def pre_checkout_process(pre_checkout: types.PreCheckoutQuery):
+#     await bot.answer_pre_checkout_query(pre_checkout.id, ok=True)
+#
+# @dp.message_handler(content_types=types.ContentType.SUCCESSFUL_PAYMENT)
+# async def successful_payment(message: types.Message):
+#     await bot.send_message(message.chat.id, 'Toʻlov muvaffaqiyatli boʻldi!')
